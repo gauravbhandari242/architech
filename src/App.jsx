5f6ccb2fe3eb5bfc440dba2bb6bedcad5a5415a8
gauravbhandari242 <gauravbhandari242@gmail.com>
2026-03-06 15:54:55 +0530

    app.css

--- src/App.jsx
+++ src/App.jsx
@@ -2,27 +2,27 @@ import React from "react";
 import "./App.css";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
-import Navbar from "./components/Header/Header"; // navbar
+import Navbar from "./components/Header/Header";
 import Component from "./components/Component/Components";
 import Service from "./components/Servicecopy/Service";
 import About from "./components/About/About";
 import Work from "./components/work/Work";
 import Working from "./components/Working/Working";
 import Blog from "./components/Blog/Blog";
-import Contact from "./components/Contact/Contact"
+import Contact from "./components/Contact/Contact";
 import Footer from "./components/FooterSection/Footer";
 import Customer from "./components/Customer_service/Customer";
 import Founder from "./Founder/Founder";
 import Aboutsss from "./components/ABOUTSSS/Aboutsss";
 
 import "./components/Header/Header.css";
-import "./components/Component/components.css";
+import "./components/Component/Components.css";
 import "./components/Servicecopy/Service.css";
 import "./components/About/About.css";
 import "./components/work/Work.css";
 import "./components/Working/Working.css";
 import "./components/Blog/Blog.css";
-import "./components/Contact/contact.css";
+import "./components/Contact/Contact.css";
 import "./components/FooterSection/Footer.css";
 import "./components/Customer_service/Customer.css";
 import "./Founder/Founder.css";
@@ -51,7 +51,8 @@ function App() {
         <Route path="/" element={<Home />} />
         <Route path="/Founder" element={<Founder />} />
         <Route path="/Servicecopy" element={<Service />} />
-        <Route path="/ABOUTSSS" element={<Work />} />
+        <Route path="/ABOUTSSS" element={<Aboutsss />} />
+        <Route path="/Contact" element={<Contact />} />
       </Routes>
 
       <Footer />
